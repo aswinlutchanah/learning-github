73b25da56c94484a26c21bf6fbf054b76de8e40d
Aswin Lutchanah <aswin@lutchanah.com>
2014-10-15 21:38:59 +0200

    inclusion du script jquery 2.1.0 (en cdn)

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8"/>
         <title>Page HTML5</title>
         <link rel="stylesheet" href="css/css.css"/>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
         <script src="js/js.js"></script>
     </head>
     <body>
