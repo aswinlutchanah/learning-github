52d5b2fe8417221fe358aefdabec4ccd68ee204f
Aswin Lutchanah <aswin@lutchanah.com>
2014-10-15 21:29:42 +0200

    rajout d'une fonction js permettant d'alerter l'utilisateur si la page contient un formulaire et que ce formulaire est rempli met qu'il a oublie de l'enegistrer et essaie de changer de page

--- js/js.js
+++ js/js.js
@@ -1,7 +1,12 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+//Unload
+if ($('form').length) {
+    $('form').change(function(e) {
+        window.onbeforeunload = confirmExit;
+        $('form').on('submit', function(e) {
+            window.onbeforeunload = '';
+        });
+        function confirmExit() {
+            return 'Vérifiez que vous avez bien enregistré avant de quitter la page.';
+        }
+    });
+}
