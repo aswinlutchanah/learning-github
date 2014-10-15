4d60ed2b59ddbc5b13cd7fa9a1801361089436ed
Aswin Lutchanah <aswin@lutchanah.com>
2014-10-15 21:34:00 +0200

    mise en forme du fichier index.php avec une structure de fichier HTML5 de base

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
-<?php
-echo 'This is a test project';
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8"/>
+        <title>Page HTML5</title>
+        <link rel="stylesheet" href="css/css.css"/>
+        <script src="js/js.js"></script>
+    </head>
+    <body>
+        <?php echo 'Bonjour ! Ceci est une page de test !'; ?>
+    </body>
+</html>
